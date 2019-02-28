44f85bd5fbb4cd87d3d54f07e15ee2e7e524a9d5
Matt Raible <matt.raible@okta.com>
2019-02-28 08:05:54 -0700

    Updates after completing QA

--- config/src/test/java/com/okta/developer/docker_microservices/config/ConfigApplicationTests.java
+++ config/src/test/java/com/okta/developer/docker_microservices/config/ConfigApplicationTests.java
@@ -1,4 +1,4 @@
-package com.okta.developer.docker_microservices.confg;
+package com.okta.developer.docker_microservices.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfgApplicationTests {
+public class ConfigApplicationTests {
 
 	@Test
 	public void contextLoads() {
